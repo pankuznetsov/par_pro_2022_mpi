9329f0bfab917cc5d1852b237a3ee9a01ebd14d2
dlapin2014 <dmitriylapin2001@gmail.com>
2022-12-26 06:30:27 +0300

    ghoul

---
+++ modules/test_tasks/test_mpi/ops_mpi.h
@@ -0,0 +1,13 @@
+// Copyright 2022 Lapin Dmitriy
+#ifndef MODULES_TASK_1_LAPIN_D_MATRIX_MINIMUM_OPS_MPI_H_
+#define MODULES_TASK_1_LAPIN_D_MATRIX_MINIMUM_OPS_MPI_H_
+#include <vector>
+#include <string>
+
+std::vector<std::vector<int>> CreateMatrix(int m, int n);
+int getParallelOperations(std::vector<std::vector<int>> matrix);
+int getSequentialOperations(std::vector<std::vector<int>> matrix);
+
+
+
+#endif  
