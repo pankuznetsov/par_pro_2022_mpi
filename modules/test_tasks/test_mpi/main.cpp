fd5ca77d63038508c12508ee497738137dc4c8c6
Nesterov Alexander <nesterov.alexander@outlook.com>
2023-02-22 15:00:47 +0100

    Revert "Лапин Дмитрий, 382003_3, Задача 2, Вариант 11, Кольцо, Пересдача "

--- modules/test_tasks/test_mpi/main.cpp
+++ modules/test_tasks/test_mpi/main.cpp
@@ -1,255 +1,102 @@
-// Copyright 2022 Lapin Dmitriy
+// Copyright 2018 Nesterov Alexander
 #include <gtest/gtest.h>
 #include <vector>
 #include "./ops_mpi.h"
 #include <gtest-mpi-listener.hpp>
 
-TEST(Parallel_Operations_MPI, Test_0) {
-    int size, rank;
-
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+TEST(Parallel_Operations_MPI, Test_Sum) {
+    int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> global_vec;
+    const int count_size_vector = 120;
 
-    if (size >= 3) {
-        MPI_Comm ring_comm;
-        int index[3] = { 0, 1, 2};
-        MPI_group_ring_create(MPI_COMM_WORLD, 3,
-            index, &ring_comm);
+    if (rank == 0) {
+        global_vec = getRandomVector(count_size_vector);
+    }
 
-        int ring_rank, ring_size;
-        if (ring_comm != MPI_COMM_NULL) {
-            MPI_Comm_rank(ring_comm, &ring_rank);
-            MPI_Comm_size(ring_comm, &ring_size);
+    int global_sum = getParallelOperations(global_vec, count_size_vector, "+");
 
-            EXPECT_LT(ring_rank, ring_size);
-            EXPECT_LT(ring_rank, 3);
-        }
-        if (ring_comm != MPI_COMM_NULL) {
-            MPI_Comm_free(&ring_comm);
-        }
+    if (rank == 0) {
+        int reference_sum = getSequentialOperations(global_vec, "+");
+        ASSERT_EQ(reference_sum, global_sum);
     }
 }
 
-
-TEST(Parallel_Operations_MPI, Test_1) {
-    int size, rank;
-
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+TEST(Parallel_Operations_MPI, Test_Diff) {
+    int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> global_vec;
+    const int count_size_vector = 120;
 
-    if (size >= 3) {
-        MPI_Comm ring_comm;
-        int index[3] = { 0, 1, 2 };
-        MPI_group_ring_create(MPI_COMM_WORLD, 3, index, &ring_comm);
+    if (rank == 0) {
+        global_vec = getRandomVector(count_size_vector);
+    }
 
-        int ring_rank, ring_size;
-        if (ring_comm != MPI_COMM_NULL) {
-            MPI_Comm_rank(ring_comm, &ring_rank);
-            MPI_Comm_size(ring_comm, &ring_size);
+    int global_diff = getParallelOperations(global_vec, count_size_vector, "-");
 
-            double check[3];
-
-            if (ring_rank == 0) {
-                for (int i = 0; i < 3; i++) {
-                    check[i] = (double)i;
-                }
-            }
-
-            Ring_Send(check, 3, MPI_DOUBLE, 0, 2, 1, ring_comm);
-
-            if (ring_rank == 2) {
-                double check2[3];
-                MPI_Status status;
-                MPI_Recv(check2, 3, MPI_DOUBLE, MPI_ANY_SOURCE, 1, ring_comm, &status);
-                for (int i = 0; i < 3; i++) {
-                    EXPECT_EQ(check2[i], i);
-                }
-            }
-        }
-
-        if (ring_comm != MPI_COMM_NULL) {
-            MPI_Comm_free(&ring_comm);
-        }
+    if (rank == 0) {
+        int reference_diff = getSequentialOperations(global_vec, "-");
+        ASSERT_EQ(reference_diff, global_diff);
     }
 }
 
-
-TEST(Parallel_Operations_MPI, Test_2) {
-    int size, rank;
-
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+TEST(Parallel_Operations_MPI, Test_Diff_2) {
+    int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> global_vec;
+    const int count_size_vector = 120;
 
-    if (size >= 3) {
-        MPI_Comm ring_comm;
-        int index[3] = { 0, 1, 2 };
-        MPI_group_ring_create(MPI_COMM_WORLD, 3, index, &ring_comm);
+    if (rank == 0) {
+        global_vec = getRandomVector(count_size_vector);
+    }
 
-        int ring_rank, ring_size;
-        if (ring_comm != MPI_COMM_NULL) {
-            MPI_Comm_rank(ring_comm, &ring_rank);
-            MPI_Comm_size(ring_comm, &ring_size);
+    int global_diff = getParallelOperations(global_vec, count_size_vector, "-");
 
-            int check[3];
-
-            if (ring_rank == 0) {
-                for (int i = 0; i < 3; i++) {
-                    check[i] = i;
-                }
-            }
-
-            Ring_Send(check, 3, MPI_INT, 0, 2, 1, ring_comm);
-
-            if (ring_rank == 2) {
-                int check2[3];
-                MPI_Status status;
-                MPI_Recv(check2, 3, MPI_INT, MPI_ANY_SOURCE, 1, ring_comm, &status);
-                for (int i = 0; i < 3; i++) {
-                    EXPECT_EQ(check2[i], i);
-                }
-            }
-        }
-
-        if (ring_comm != MPI_COMM_NULL) {
-            MPI_Comm_free(&ring_comm);
-        }
+    if (rank == 0) {
+        int reference_diff = getSequentialOperations(global_vec, "-");
+        ASSERT_EQ(reference_diff, global_diff);
     }
 }
 
-
-TEST(Parallel_Operations_MPI, Test_3) {
-    int size, rank;
-
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+TEST(Parallel_Operations_MPI, Test_Max) {
+    int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> global_vec;
+    const int count_size_vector = 120;
 
-    if (size >= 3) {
-        MPI_Comm ring_comm;
-        int index[3] = { 0, 1, 2 };
-        MPI_group_ring_create(MPI_COMM_WORLD, 3, index, &ring_comm);
+    if (rank == 0) {
+        global_vec = getRandomVector(count_size_vector);
+    }
 
-        int ring_rank, ring_size;
-        if (ring_comm != MPI_COMM_NULL) {
-            MPI_Comm_rank(ring_comm, &ring_rank);
-            MPI_Comm_size(ring_comm, &ring_size);
+    int global_max;
+    global_max = getParallelOperations(global_vec, count_size_vector, "max");
 
-            char check[3];
-
-            if (ring_rank == 0) {
-                for (int i = 0; i < 3; i++) {
-                    check[i] = 'a';
-                }
-            }
-
-            Ring_Send(check, 3, MPI_CHAR, 0, 2, 1, ring_comm);
-
-            if (ring_rank == 2) {
-                char check2[3];
-                MPI_Status status;
-                MPI_Recv(check2, 3, MPI_CHAR, MPI_ANY_SOURCE, 1, ring_comm, &status);
-                for (int i = 0; i < 3; i++) {
-                    EXPECT_EQ(check2[i], 'a');
-                }
-            }
-        }
-
-        if (ring_comm != MPI_COMM_NULL) {
-            MPI_Comm_free(&ring_comm);
-        }
+    if (rank == 0) {
+        int reference_max = getSequentialOperations(global_vec, "max");
+        ASSERT_EQ(reference_max, global_max);
     }
 }
 
-
-
-
-TEST(Parallel_Operations_MPI, Test_4) {
-    int size, rank;
-
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+TEST(Parallel_Operations_MPI, Test_Max_2) {
+    int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> global_vec;
+    const int count_size_vector = 120;
 
-    if (size >= 4) {
-        MPI_Comm ring_comm;
-        int index[4] = { 0, 1, 2, 3 };
-        MPI_group_ring_create(MPI_COMM_WORLD, 4, index, &ring_comm);
+    if (rank == 0) {
+        global_vec = getRandomVector(count_size_vector);
+    }
 
-        int ring_rank, ring_size;
-        if (ring_comm != MPI_COMM_NULL) {
-            MPI_Comm_rank(ring_comm, &ring_rank);
-            MPI_Comm_size(ring_comm, &ring_size);
+    int global_max;
+    global_max = getParallelOperations(global_vec, count_size_vector, "max");
 
-            int check[3];
-
-            if (ring_rank == 0) {
-                for (int i = 0; i < 3; i++) {
-                    check[i] = i;
-                }
-            }
-
-            Ring_Send(check, 3, MPI_INT, 0, 3, 1, ring_comm);
-
-            if (ring_rank == 3) {
-                int check2[3];
-                MPI_Status status;
-                MPI_Recv(check2, 3, MPI_INT, MPI_ANY_SOURCE, 1, ring_comm, &status);
-                for (int i = 0; i < 3; i++) {
-                    EXPECT_EQ(check2[i], i);
-                }
-            }
-        }
-
-        if (ring_comm != MPI_COMM_NULL) {
-            MPI_Comm_free(&ring_comm);
-        }
+    if (rank == 0) {
+        int reference_max = getSequentialOperations(global_vec, "max");
+        ASSERT_EQ(reference_max, global_max);
     }
 }
-
-TEST(Parallel_Operations_MPI, Test_5) {
-    int size, rank;
-
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-    if (size >= 6) {
-        MPI_Comm ring_comm;
-        int index[6] = { 0, 1, 2, 3, 4 ,5 };
-        MPI_group_ring_create(MPI_COMM_WORLD, 6, index, &ring_comm);
-
-        int ring_rank, ring_size;
-        if (ring_comm != MPI_COMM_NULL) {
-            MPI_Comm_rank(ring_comm, &ring_rank);
-            MPI_Comm_size(ring_comm, &ring_size);
-
-            int check[3];
-
-            if (ring_rank == 0) {
-                for (int i = 0; i < 3; i++) {
-                    check[i] = i;
-                }
-            }
-
-            Ring_Send(check, 3, MPI_INT, 0, 4, 1, ring_comm);
-
-            if (ring_rank == 4) {
-                int check2[3];
-                MPI_Status status;
-                MPI_Recv(check2, 3, MPI_INT, MPI_ANY_SOURCE, 1, ring_comm, &status);
-                for (int i = 0; i < 3; i++) {
-                    EXPECT_EQ(check2[i], i);
-                }
-            }
-        }
-
-        if (ring_comm != MPI_COMM_NULL) {
-            MPI_Comm_free(&ring_comm);
-        }
-    }
-}
-
-
 
 int main(int argc, char** argv) {
-
     ::testing::InitGoogleTest(&argc, argv);
     MPI_Init(&argc, &argv);
 
--- modules/test_tasks/test_mpi/ops_mpi.cpp
+++ modules/test_tasks/test_mpi/ops_mpi.cpp
@@ -1,132 +1,68 @@
-// Copyright 2022 Lapin Dmitriy
+// Copyright 2018 Nesterov Alexander
 #include <mpi.h>
 #include <vector>
-#include <iostream>
-#include <list>
-#include "./ops_mpi.h"
-using namespace std;
+#include <string>
+#include <random>
+#include <algorithm>
+#include "../../../modules/test_tasks/test_mpi/ops_mpi.h"
 
 
-void MPI_group_ring_create(MPI_Comm old_comm, int nodes,
-    int index[], MPI_Comm* new_comm) {
+std::vector<int> getRandomVector(int sz) {
+    std::random_device dev;
+    std::mt19937 gen(dev());
+    std::vector<int> vec(sz);
+    for (int  i = 0; i < sz; i++) { vec[i] = gen() % 100; }
+    return vec;
+}
 
+int getSequentialOperations(std::vector<int> vec, const std::string& ops) {
+    const int  sz = vec.size();
+    int reduction_elem = 0;
+    if (ops == "+") {
+        for (int  i = 0; i < sz; i++) {
+            reduction_elem += vec[i];
+        }
+    } else if (ops == "-") {
+        for (int  i = 0; i < sz; i++) {
+            reduction_elem -= vec[i];
+        }
+    } else if (ops == "max") {
+        reduction_elem = vec[0];
+        for (int  i = 1; i < sz; i++) {
+            reduction_elem = std::max(reduction_elem, vec[i]);
+        }
+    }
+    return reduction_elem;
+}
+
+int getParallelOperations(std::vector<int> global_vec,
+                          int count_size_vector, const std::string& ops) {
     int size, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    const int delta = count_size_vector / size;
 
-    MPI_Comm_size(old_comm, &size);
-    MPI_Comm_rank(old_comm, &rank);
+    if (rank == 0) {
+        for (int proc = 1; proc < size; proc++) {
+            MPI_Send(global_vec.data() + proc * delta, delta,
+                        MPI_INT, proc, 0, MPI_COMM_WORLD);
+        }
+    }
 
-    MPI_Group old_group;
-    MPI_Comm_group(old_comm, &old_group);
+    std::vector<int> local_vec(delta);
+    if (rank == 0) {
+        local_vec = std::vector<int>(global_vec.begin(),
+                                     global_vec.begin() + delta);
+    } else {
+        MPI_Status status;
+        MPI_Recv(local_vec.data(), delta, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+    }
 
-    MPI_Group ring_group;
-    MPI_Group_incl(old_group, nodes, index, &ring_group);
-
-    MPI_Comm ring_comm;
-    MPI_Comm_create(old_comm, ring_group, &ring_comm);
-
-    MPI_Group_free(&old_group);
-
-    *new_comm = ring_comm;
+    int global_sum = 0;
+    int local_sum = getSequentialOperations(local_vec, ops);
+    MPI_Op op_code = MPI_OP_NULL;
+    if (ops == "+" || ops == "-") { op_code = MPI_SUM; }
+    if (ops == "max") { op_code = MPI_MAX; }
+    MPI_Reduce(&local_sum, &global_sum, 1, MPI_INT, op_code, 0, MPI_COMM_WORLD);
+    return global_sum;
 }
-
-
-list<int> Dejkstra(int n, int ST, int dest)
-{
-
-    vector<vector<int>> matrix(n);
-    for (int i = 0; i < n; i++)
-        matrix[i].resize(n);
-
-    for (int i = 0; i < n - 1; i++) {
-        matrix[i][i + 1] = matrix[i + 1][i] = 1;
-    }
-    matrix[0][n - 1] = matrix[n - 1][0] = 1;
-
-    int infin = 1000000;
-
-    vector <int> d(n, infin);
-    vector <bool> s(n, false);
-    vector <list<int>> path(n);
-
-    d[ST] = 0;
-    s[ST] = true;
-
-    int o = ST;
-
-    for (int j = 0; j < n - 1; j++) {
-
-        for (int i = 0; i < n; i++) {
-            if (s[i] == false && matrix[o][i] != 0 && d[o] + matrix[o][i] < d[i]) {
-                d[i] = d[o] + matrix[o][i];
-                path[i] = path[o];
-                path[i].push_back(i);
-            }
-        }
-
-
-        int min = infin;
-        for (int i = 0; i < n; i++) {
-            if (s[i] == false && d[i] < min) {
-                min = d[i];
-                o = i;
-            }
-        }
-
-        s[o] = true;
-
-    }
-    return path[dest];
-}
-
-
-int Ring_Send(const void* buf, int count, MPI_Datatype datatype,
-    int from, int dest, int tag, MPI_Comm comm) {
-    if (comm != MPI_COMM_NULL) {
-        int size, rank;
-
-        MPI_Comm_rank(comm, &rank);
-        MPI_Comm_size(comm, &size);
-
-        if (rank == from) {
-            
-            list<int> t = Dejkstra(size, from, dest);
-            vector<int> routeTwo(size, -1);
-            auto it = t.begin();
-            auto at = ++t.begin();
-            routeTwo[from] = *it;
-            for (;at != t.end(); it++, at++) {
-                routeTwo[*it] = *at;
-            }
-
-            for (int i = 0; i < size; i++) {
-                if (i != from) {
-                    MPI_Send(&routeTwo[i], 1, MPI_INT, i, tag, comm);
-                }
-            }
-            MPI_Send(buf, count, datatype, routeTwo[rank], tag, comm);
-
-            return 0;
-        }
-        else {
-            MPI_Status status;
-            int d;
-            MPI_Recv(&d, 1, MPI_INT, from, MPI_ANY_TAG, comm, &status);
-
-            if (d != -1) {
-                int size_of_datatype;
-                MPI_Type_size(datatype, &size_of_datatype);
-                std::vector<char> local_vec(size_of_datatype * count);
-                MPI_Recv(local_vec.data(), count, datatype, MPI_ANY_SOURCE, tag, comm, &status);
-                MPI_Send(local_vec.data(), count, datatype, d, tag, comm);
-            }
-            return 0;
-        }
-        return 0;
-    }
-        
-    else {
-        return -1;
-    }
-    return 0;
-}
-
